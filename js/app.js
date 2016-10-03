61cf6d319dad7f3669e0f6d7b0d5ebf6cf560d87
Seb Blaszczyk <seb.blaszczyk@hotmail.com>
2016-10-03 21:34:14 +1100

    Refactoring JavaScript and initial styles

--- js/app.js
+++ js/app.js
@@ -1,108 +1,50 @@
 'use strict';
-//IIF to start things off
-(function() {
-  // Set Global Variables
-  var httpRequest, data, teamsList = [], teamsListOutput, competitionsId, competitionsList = [], i, competitionsListOutput, apiKey = "f442eb0d27b3477d83f81e414572e2f2";
-  
-  // Make request for Data
-  function makeRequest(url,getData) {
-    httpRequest = new XMLHttpRequest();
 
-    if (!httpRequest) {
-      alert('Giving up :( Cannot create an XMLHTTP instance');
-      return false;
-    }
-    httpRequest.onreadystatechange = getData; // Have a closer look
-    httpRequest.open('GET', url);
-    httpRequest.setRequestHeader("X-Auth-Token",apiKey);
-    httpRequest.send();
-  }
-  
-  // Check state of request and if successful get data
-  function competitionsData() {
-    if (httpRequest.readyState === XMLHttpRequest.DONE) {
-      if (httpRequest.status === 200) {
-        data = JSON.parse(httpRequest.responseText);
-        for(i=0;i<data.length;i++){
-	        competitionsList.push('<li class="league" data-leagueid="'+data[i].id+'">'+data[i].caption+'</li>');
-	    }
-	    competitionsList.push('</ul>');
-	    competitionsList.unshift('<ul id="competitions-list">');
-	    competitionsListOutput = competitionsList.join("");
-	    document.getElementById("competitions").innerHTML = competitionsListOutput;
-	    var parent = document.getElementById('competitions-list');
-		parent.addEventListener('click', function (e) {
-			this.className = ' hidden';
-			document.getElementById('clubs').innerHTML = '<button id="back-to-competitions">Go back and select another Competition</button><h2>'+e.target.textContent+'</h2>';
-		    makeRequest('http://api.football-data.org/v1/competitions/'+e.target.dataset.leagueid+'/teams',teamData);
-		    document.getElementById('back-to-competitions').addEventListener('click', function(e) { 
-				parent.className = '';
-				document.getElementById('clubs').innerHTML = '';
-				document.getElementById('teams').innerHTML = '';
-			});
-		});
-		
-		
-	   } else {
-        alert('There was a problem with the request.');
-      }
-    }
-  }
-  
-  // Team Data
-  function teamData() {
-    if (httpRequest.readyState === XMLHttpRequest.DONE) {
-      if (httpRequest.status === 200) {
-	    teamsList = [];  
-        var teData = JSON.parse(httpRequest.responseText);
-        var te;
-        for(te=0;te<teData.teams.length;te++){
-	        teamsList.push('<li class="club" data-badge="'+teData.teams[te].crestUrl+'" data-fixtures="'+teData.teams[te]._links.fixtures.href+'">'+teData.teams[te].name+'</li>');
-	    }
-	    teamsList.push('</ul>');
-	    teamsList.unshift('<ul id="teams-list">');
-	    teamsListOutput = teamsList.join("");
-	    document.getElementById("teams").innerHTML = teamsListOutput;
-	    var teamParent = document.getElementById('teams-list');
-		teamParent.addEventListener('click', function (e) {
-			this.className = ' hidden';
-			document.getElementById('clubs').innerHTML = '';
-			document.getElementById('teams').innerHTML = '<h2>'+e.target.textContent+'</h2>';
-		    makeRequest(e.target.dataset.fixtures,fixturesData);
-		});
-        
-	   } else {
-        alert('There was a problem with the request.');
-      }
-    }
-  }
-  
-  // Fixtures Data
-  function fixturesData() {
-	 if (httpRequest.readyState === XMLHttpRequest.DONE) {
-      if (httpRequest.status === 200) {
-        var fdata = JSON.parse(httpRequest.responseText);
-        var fd;
-        var fixturesList = [];
-        for(fd=0;fd<fdata.fixtures.length;fd++){
-	        fixturesList.push('<div class="fixture"><h3>Matchday '+fdata.fixtures[fd].matchday+'</h3><span class="home-team">'+fdata.fixtures[fd].homeTeamName+'</span> vs <span class="away-team">'+fdata.fixtures[fd].awayTeamName+'</span></div>');
-	    }
-        document.getElementById("fixtures").innerHTML = fixturesList.join("");
-        
-	   } else {
-        alert('There was a problem with the request.');
-      }
-    } 
-  }
+/*, data, teamsList = [], teamsListOutput, competitionsId, competitionsList = [], i, competitionsListOutput, apiKey = "f442eb0d27b3477d83f81e414572e2f2";*/
+
+var App = {};
+
+App.settings = {
+	accessToken: function() { return localStorage.getItem('accessKey'); },
+	dataUrl: "http://api.football-data.org/v1"
+};
+
+App.getData = function(url) {
+	// Return a new promise.
+	return new Promise(function(resolve, reject) {
+	    // Do the usual XHR stuff
+	    var httpRequest = new XMLHttpRequest();
+	    httpRequest.open('GET', url);
+	    httpRequest.setRequestHeader("X-Auth-Token",App.settings.accessToken());
+	
+	    httpRequest.onload = function() {
+	      // This is called even on 404 etc
+	      // so check the status
+	      if (httpRequest.status == 200) {
+	        // Resolve the promise with the response text
+	        resolve(httpRequest.response);
+	      }
+	      else {
+	        // Otherwise reject with the status text
+	        // which will hopefully be a meaningful error
+	        reject(Error(httpRequest.statusText));
+	      }
+	    };
+	
+	    // Handle network errors
+	    httpRequest.onerror = function() {
+	      reject(Error("Network Error"));
+	    };
+	
+	    // Make the request
+	    httpRequest.send();
+	});	
+};
+
+	 
+
+console.log('App Loaded');  
   
   
   
  
-  // Controller - User Triggers (view.js file?)
-  document.getElementById('get-competitions').onclick = function() { 
-	  makeRequest('http://api.football-data.org/v1/competitions/?season=2016',competitionsData); 
-	  this.className = ' hidden';
-  };
-  					 
-			  
-})();
---
+++ js/control.js
@@ -0,0 +1,91 @@
+function loadScript(url,callback){var script=document.createElement("script")
+script.type="text/javascript";if(script.readyState){script.onreadystatechange=function(){if(script.readyState=="loaded"||script.readyState=="complete"){script.onreadystatechange=null;callback();}};}else{script.onload=function(){callback();};}
+script.src=url;document.body.appendChild(script);}
+
+loadScript("js/app.js", function(){
+	
+	// Set Global Variables
+	var dataUrl = "http://api.football-data.org/v1", 
+	competitionData, 
+	competitionListUl = document.getElementById('competitions-list'),
+	competitionsList = [],
+	clubData,
+	clubHtmlContainer =  document.getElementById('clubs'),
+	clubListUl = document.getElementById('clubs-list'),
+	clubList = [],
+	clubListOutput,
+	backToCompetitions = document.getElementById('back-to-competitions'),
+	chosenClubData,
+	chosenClubFixturesList = [],
+	fixturesHtmlContainer = document.getElementById("fixtures"),
+	hiddenClass = ' hidden';
+	
+	// Get Competitions List
+  	document.getElementById('get-competitions').onclick = function() { 
+	  	App.getData(dataUrl+"/competitions/?season=2016").then(function(response) {
+		  	competitionData = JSON.parse(response);
+		  	for(var i=0;i<competitionData.length;i++){
+	        	competitionsList.push('<li class="league" data-leagueid="'+competitionData[i].id+'">'+competitionData[i].caption+'</li>');
+		    }
+		    competitionsListOutput = competitionsList.join("");
+		    competitionListUl.innerHTML = competitionsListOutput;
+		    
+		    // Store Competition Id's
+		    console.log(competitionData);
+		    
+		}, function(error) {
+			console.error("Failed!", error);
+		});
+		this.className = hiddenClass;
+	};  
+	
+	// Select a Competition and Load Teams
+	competitionListUl.addEventListener('click', function (e) {
+		this.className = 'options-list '+hiddenClass+'';
+		clubHtmlContainer.querySelector(".club-title").innerHTML = e.target.textContent;
+		clubHtmlContainer.className = '';
+		App.getData(dataUrl+'/competitions/'+e.target.dataset.leagueid+'/teams').then(function(response) {
+		  	clubData = JSON.parse(response);
+	        for(var i=0;i<clubData.teams.length;i++){
+		        clubList.push('<li class="club" data-badge="'+clubData.teams[i].crestUrl+'" data-fixtures="'+clubData.teams[i]._links.fixtures.href+'">'+clubData.teams[i].name+'<button class="view-club small-button button grey-button">View</button><button class="follow-club small-button button">Follow</button></li>');
+		    }
+		    clubListOutput = clubList.join("");
+		    clubListUl.innerHTML = clubListOutput;
+		}, function(error) {
+			console.error("Failed!", error);
+		});
+	});
+	
+	// Go back to Competition Selection
+	backToCompetitions.addEventListener('click', function(e) { 
+		competitionListUl.className = 'options-list';
+		clubHtmlContainer.className = 'hidden';
+		clubList.length = 0;
+		clubListUl.innerHTML = '';
+	});
+	
+	// Follow a Club
+	document.querySelector(".follow-club").addEventListener('click', function (e) {
+		console.log(this.parentNode);
+	});
+	
+	/*clubListUl.addEventListener('click', function (e) {
+		this.className = ' hidden';
+		clubHtmlContainer.innerHTML = '<h2>'+e.target.textContent+'</h2>';
+		App.getData(e.target.dataset.fixtures).then(function(response) {
+			var chosenClubData = JSON.parse(response);
+	        for(var i=0;i<chosenClubData.fixtures.length;i++){
+		        chosenClubFixturesList.push('<div class="fixture"><h3>Matchday '+chosenClubData.fixtures[i].matchday+'</h3><span class="home-team">'+chosenClubData.fixtures[i].homeTeamName+'</span> vs <span class="away-team">'+chosenClubData.fixtures[i].awayTeamName+'</span></div>');
+		    }
+	        fixturesHtmlContainer.innerHTML = chosenClubFixturesList.join("");
+		}, function(error) {
+			console.error("Failed!", error);
+		});
+	}); */
+	
+	
+			
+});
+
+
+
